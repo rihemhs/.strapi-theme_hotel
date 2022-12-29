768909a36d5f17b7a3cba17696950067e3aaf26c
unknown <rihemhassounanjim90@gmail.com>
2022-12-29 15:34:04 +0100

    Strapi-theme

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', ''),
+      
+    },
+    debug: false,
+  },
+});
